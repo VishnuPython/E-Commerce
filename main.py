7267c6a4c35f2ca4677caae98913b92c0e156733
VishnuPython <140088047+VishnuPython@users.noreply.github.com>
2024-02-04 11:16:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,68 @@
-from flask import Flask,jsonify
-from flask_mysqldb import MySQL
-from flask_cors import CORS,cross_origin
+from fastapi import FastAPI
+import mysql.connector
 
-#Add mysql connection values in lines 8-12
+app=FastAPI()
 
-app=Flask(__name__)
-app.config['MYSQL_HOST']=''
-app.config['MYSQL_USER']=''
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_PORT']=0
-app.config['MYSQL_DB']=''
-mysql=MySQL(app)
-cors=CORS(app)
-
-class Main:
-    def products(count):
-        cur=mysql.connection.cursor()
-        ID=1001
-        products_json=[]
-        for product in range(count):
-            cur.execute(f"""select * from products where id={ID}""")
-            data=cur.fetchone()
-            products_count=0
-            for i in data:
-                products={'id':data[0],'title':data[1],'price':data[2],'imagePath':data[3]}
-            products_json.append(products)
-            ID=ID+1
+class Mysql:
+    def __init__(self,host,user,password,database):
+        self.host=host
+        self.user=user
+        self.password=password
+        self.database=database
+    def connection(self,query):
+        con=mysql.connector.connect(host=self.host,user=self.user,password=self.password,database=self.database)
+        cur=con.cursor()
+        cur.execute(query)
+        data=cur.fetchall()
         cur.close()
-        return products_json
-        
+        con.close()
+        return data
+    def connection_commit(self,query):
+        con=mysql.connector.connect(host=self.host,user=self.user,password=self.password,database=self.database)
+        cur=con.cursor()
+        cur.execute(query)
+        data=cur.commit()
+        cur.close()
+        con.close()
+        return data
 
-@app.route('/products',methods=['GET'])
-@cross_origin()
-def main():
-    products=Main.products(7)
-    return jsonify(products)
+class Products(Mysql):
+    def __init__(self):
+        super().__init__("localhost","root","password","db")
+    def products(self):
+        products=[]
+        query="""select * from products"""
+        product_data=self.connection(query=query)
+        for data in product_data:
+            product_json={"id":data[0],"title":data[1],"price":data[2],"image_path":data[3]}
+            products.append(product_json)
+        return products
+    def categories(self,category):
+        query="""select * from products where category="%s" """
+        data=self.connection(query,category)
+        return data
+
+class Profile(Mysql):
+    def __init__(self,Id,email,password):
+        super().__init__("localhost","root","password","db")
+        self.Id=Id
+        self.email=email
+        self.password=password
+    def create_user(self):
+        query="""insert into users(id,email,password) values(%s,%s,%s)"""
+        data=(self.Id,self.email,self.password)
+        self.connection_commit(query,data)
+        return data
+    
+
+    
+    
+@app.get("/")
+async def products():
+    products=Products()
+    return products.products()
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
